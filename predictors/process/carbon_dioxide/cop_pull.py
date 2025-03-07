9672b08867734c98cb8706142fb781cabf79203b
EdSacre <88026687+EdSacre@users.noreply.github.com>
2025-03-07 07:22:50 +0100

    Update predictors

---
+++ predictors/process/carbon_dioxide/cop_pull.py
@@ -0,0 +1,24 @@
+import copernicusmarine
+
+r = [str(i) for i in range(2010,2024, 1)]
+
+for i in r:
+    yr = i
+    st = yr + "-01-01T00:00:00"
+    et = yr + "-12-31T23:59:59"
+    of = "cmems_mod_bal_bgc_" + yr + ".nc"
+    od = "inputs/temp"
+  
+    copernicusmarine.subset(
+      dataset_id="cmems_mod_bal_bgc_my_P1M-m",
+      variables=["spco2"],
+      minimum_longitude=9.041532516479492,
+      maximum_longitude=30.20798683166504,
+      minimum_latitude=53.00829315185547,
+      maximum_latitude=65.89141845703125,
+      start_datetime=st,
+      end_datetime=et,
+      output_filename = of,
+      output_directory = od
+    )
+
