90d8426aab997e6364301b79480c745331fc3baa
EdSacre <88026687+EdSacre@users.noreply.github.com>
2025-03-08 20:31:13 +0100

    Update predictors

---
+++ predictors/process/temperature_satellite/cop_pull.py
@@ -0,0 +1,24 @@
+import copernicusmarine
+
+r = [str(i) for i in range(2010,2024, 1)]
+
+for i in r:
+    yr = i
+    st = yr + "-01-01T00:00:00"
+    et = yr + "-12-31T23:59:59"
+    of = "DMI_BAL_SST_" + yr + ".nc"
+    od = "inputs/temp"
+  
+    copernicusmarine.subset(
+      dataset_id="DMI_BAL_SST_L4_REP_OBSERVATIONS_010_016",
+      variables=["analysed_sst"],
+      minimum_longitude=-10,
+      maximum_longitude=30,
+      minimum_latitude=48,
+      maximum_latitude=66,
+      start_datetime=st,
+      end_datetime=et,
+      output_filename = of,
+      output_directory = od
+    )
+
