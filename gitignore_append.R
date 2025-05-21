aa034d3c1799b955a3fde0b45c3599eb84d46bb2
EdSacre <88026687+EdSacre@users.noreply.github.com>
2025-05-21 07:31:31 +0200

    Update gitignore

--- gitignore_append.R
+++ gitignore_append.R
@@ -1,5 +1,6 @@
 l <- list.files(full.names = TRUE, recursive = TRUE)
 l <- l[sapply(l, file.size) >= 100*1000000] # MB
+l <- gsub("^\\./", "", l)
 gitfile <- file(".gitignore", open = "a")
 writeLines(l, gitfile)
 close(gitfile)
